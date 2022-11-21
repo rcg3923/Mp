4200519691389225542118f3ede48df412275d18
Zenonylon <zenonylon@gmail.com>
2022-11-21 16:37:25 +0900

    just add all friends in friendlist in MainFragment

--- app/src/main/java/com/example/mobilepro/MainFragment.java
+++ app/src/main/java/com/example/mobilepro/MainFragment.java
@@ -6,17 +6,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.mobilepro.Account.UserAccount;
 import com.example.mobilepro.MainActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// 친구창 프래그먼트
 public class MainFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
-
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.peoplefragment_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(inflater.getContext()));
+        recyclerView.setAdapter(new PeopleFragmentRecyclerViewAdapter());
 
 
         ImageButton btn_chat = rootView.findViewById(R.id.button_chat);
@@ -49,5 +65,62 @@ public class MainFragment extends Fragment {
 
 
         return rootView;
+    }
+
+    // recyclerview 추가
+    class PeopleFragmentRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        List<UserAccount> userAccounts;  // 우리 코드엔 useraccount로 적용하면 될 듯
+        public PeopleFragmentRecyclerViewAdapter() {
+            userAccounts = new ArrayList<>();
+            FirebaseDatabase.getInstance().getReference().child("UserAccount").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        userAccounts.add(snapshot.getValue(UserAccount.class));
+                    }
+                    notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_friend, parent, false);
+
+            return new CustomViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            ((CustomViewHolder)holder).nameView.setText(userAccounts.get(position).getName());
+            ((CustomViewHolder)holder).emailView.setText(userAccounts.get(position).getEmailId());
+        }
+
+        @Override
+        public int getItemCount() {
+            return userAccounts.size();
+        }
+
+        private class CustomViewHolder extends RecyclerView.ViewHolder {
+            public ImageView imageView;
+            public TextView nameView;
+            public TextView emailView;
+
+            public CustomViewHolder(View view) {
+                super(view);
+                imageView = (ImageView) view.findViewById(R.id.iv_user);
+                nameView = (TextView) view.findViewById(R.id.tv_username);
+                emailView = (TextView) view.findViewById(R.id.tv_useremail);
+            }
+        }
+
+
     }
 }
