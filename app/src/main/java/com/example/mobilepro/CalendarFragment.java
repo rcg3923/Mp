25f7a9f3b8e33170b0d13e689a848f2cdc2325aa
Kimmyeonghwan <myeonghwan2@kookmin.ac.kr>
2022-11-21 16:55:20 +0900

    Add Calendar UI & Calendar Func.(Add Memo -> Save internal storage)

--- app/src/main/java/com/example/mobilepro/CalendarFragment.java
+++ app/src/main/java/com/example/mobilepro/CalendarFragment.java
@@ -1,5 +1,6 @@
 package com.example.mobilepro;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +11,41 @@ import android.widget.ImageButton;
 import androidx.fragment.app.Fragment;
 
 import com.example.mobilepro.MainActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class CalendarFragment extends Fragment {
+    public String fname=null;
+    public String str=null;
+    public CalendarView calendarView;
+    public Button change_Btn,del_Btn,save_Btn;
+    public TextView title, diaryText, contextView;
+    public EditText contextEditText;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        title = rootView.findViewById(R.id.title);
+        calendarView = rootView.findViewById(R.id.calendar);
+        diaryText = rootView.findViewById(R.id.diaryText);
+        save_Btn = rootView.findViewById(R.id.save_Btn);
+        del_Btn = rootView.findViewById(R.id.del_Btn);
+        change_Btn = rootView.findViewById(R.id.change_Btn);
+        contextView = rootView.findViewById(R.id.contextView);
+        contextEditText = rootView.findViewById(R.id.contextEditText);
 
         ImageButton btn_main = rootView.findViewById(R.id.button_main);
         ImageButton btn_chat = rootView.findViewById(R.id.button_chat);
@@ -44,7 +74,97 @@ public class CalendarFragment extends Fragment {
             }
         });
 
-
         return rootView;
+    }
+
+    public void checkDay(int cYear, int cMonth, int cDay)
+    {
+        /* 저장할 파일 이름 설정 */
+        fname = "" + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt";
+        FileInputStream fis = null;
+
+        try{
+            fis = getContext().openFileInput(fname);
+
+            byte[] fileData=new byte[fis.available()];
+            fis.read(fileData);
+            fis.close();
+
+            str=new String(fileData);
+
+            contextEditText.setVisibility(View.INVISIBLE);
+            contextView.setVisibility(View.VISIBLE);
+            contextView.setText(str);
+            save_Btn.setVisibility(View.INVISIBLE);
+            change_Btn.setVisibility(View.VISIBLE);
+            del_Btn.setVisibility(View.VISIBLE);
+
+            change_Btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    contextEditText.setVisibility(View.VISIBLE);
+                    contextView.setVisibility(View.INVISIBLE);
+                    contextEditText.setText(str);
+
+                    save_Btn.setVisibility(View.VISIBLE);
+                    change_Btn.setVisibility(View.INVISIBLE);
+                    del_Btn.setVisibility(View.INVISIBLE);
+                    contextView.setText(contextEditText.getText());
+                }
+            });
+
+            del_Btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    contextView.setVisibility(View.INVISIBLE);
+                    contextEditText.setText("");
+                    contextEditText.setVisibility(View.VISIBLE);
+                    save_Btn.setVisibility(View.VISIBLE);
+                    change_Btn.setVisibility(View.INVISIBLE);
+                    del_Btn.setVisibility(View.INVISIBLE);
+                    removeDiary(fname);
+                }
+            });
+
+            if(contextView.getText() == null){
+                contextView.setVisibility(View.INVISIBLE);
+                diaryText.setVisibility(View.VISIBLE);
+                save_Btn.setVisibility(View.VISIBLE);
+                change_Btn.setVisibility(View.INVISIBLE);
+                del_Btn.setVisibility(View.INVISIBLE);
+                contextEditText.setVisibility(View.VISIBLE);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    public void removeDiary(String readDay){
+        FileOutputStream fos=null;
+
+        try{
+            fos = getContext().openFileOutput(readDay, Context.MODE_NO_LOCALIZED_COLLATORS);
+            String content="";
+            fos.write((content).getBytes());
+            fos.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @SuppressLint("WrongConstant")
+    public void saveDiary(String readDay){
+        FileOutputStream fos = null;
+
+        try{
+            fos = getContext().openFileOutput(readDay, Context.MODE_NO_LOCALIZED_COLLATORS);
+            String content = contextEditText.getText().toString();
+            fos.write((content).getBytes());
+            fos.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
